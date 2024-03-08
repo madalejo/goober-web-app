055211beb200049fc80d7129d2ca39ad570a09ce
Alejandro <delgadillo.alde@gmail.com>
2024-03-07 18:12:11 -0600

    Add MUI router provider

--- src/app/rider/layout.tsx
+++ src/app/rider/layout.tsx
@@ -1,4 +1,8 @@
+'use client'
 import { JSX, ReactNode } from "react"
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter"
+
+
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
 
 // Navigation Components
@@ -6,15 +10,16 @@ import AppBar from "@/app/ui/rider/app-bar"
 import NavBar from "@/app/ui/rider/nav-bar"
 
 const Layout = ({ children } : { children: ReactNode}): JSX.Element => {
+
     return (
         <Grid container justifyContent="center">
             <Grid xs={12}>
                 <AppBar />
             </Grid>
             <Grid xs={12} md={4} sx={{ m: 2 }}>
-                <>
+                <AppRouterCacheProvider>
                     { children }
-                </>
+                </AppRouterCacheProvider>
             </Grid>
             <Grid xs={12}>
                 <NavBar />
