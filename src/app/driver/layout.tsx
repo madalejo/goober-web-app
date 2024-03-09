a0c5e3ab53d2c74dd78b274e1044f42074fefbb9
Alejandro <delgadillo.alde@gmail.com>
2024-03-08 20:11:50 -0600

    Add driver layout

---
+++ src/app/driver/layout.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { JSX, ReactNode } from "react"
+
+import Grid from "@mui/material/Unstable_Grid2/Grid2"
+
+// Navigation Components
+import AppBar from "@/app/ui/rider/app-bar"
+import NavBar from "@/app/ui/rider/nav-bar"
+
+const Layout = ({ children } : { children: ReactNode}): JSX.Element => {
+
+    return (
+        <Grid container justifyContent="center">
+            <Grid xs={12}>
+                <AppBar />
+            </Grid>
+            <Grid xs={12} md={4} sx={{ m: 2 }}>
+                { children }
+            </Grid>
+            <Grid xs={12}>
+                <NavBar />
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Layout
