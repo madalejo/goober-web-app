97a3b044c2fb92952501a8c9715385dca533742e
Alejandro <delgadillo.alde@gmail.com>
2024-03-09 08:49:05 -0600

    Add suspense loader

--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -1,8 +1,9 @@
-import { FC } from "react"
+import { FC, Suspense } from "react"
 
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
 import ActiveRidesWrap from "@/app/ui/driver/active-rides-wrap"
 import ActiveRides from "@/app/ui/driver/active-rides"
+import { CurrentRideSkeletong } from "@/app/ui/skeletons"
 
 const Page: FC = (): JSX.Element => {
     return (
@@ -12,7 +13,9 @@ const Page: FC = (): JSX.Element => {
         >
             <Grid xs={12}>
                 <ActiveRidesWrap>
-                    <ActiveRides />
+                    <Suspense fallback={<CurrentRideSkeletong />}>
+                        <ActiveRides />
+                    </Suspense>
                 </ActiveRidesWrap>
             </Grid>
         </Grid>
